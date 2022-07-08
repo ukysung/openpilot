8860a55586d7fa1ffd326c64da7139d66147aed3
Sung, Wookyung <ukysung@gmail.com>
2022-07-08 21:21:12 +0900

    remove lowBattery

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -195,10 +195,7 @@ class Controls:
       self.events.add(EventName.controlsInitializing)
       return
 
-    # Create events for battery, temperature, disk space, and memory
-    if EON and self.sm['deviceState'].batteryPercent < 1 and self.sm['deviceState'].chargingError:
-      # at zero percent battery, while discharging, OP should not allowed
-      self.events.add(EventName.lowBattery)
+    # Create events for temperature, disk space, and memory
     if self.sm['deviceState'].thermalStatus >= ThermalStatus.red:
       self.events.add(EventName.overheat)
     if self.sm['deviceState'].freeSpacePercent < 7 and not SIMULATION:
